0aec20101e770f2dcaff0597748568fa9ee698bf
TheYoungWolf <68595463+TheYoungWolf-Productions@users.noreply.github.com>
2022-01-03 10:37:44 +0500

    Add control variables

--- bluetooth_controlled_servo.c
+++ bluetooth_controlled_servo.c
@@ -1,2 +1,6 @@
 #include<SoftwareSerial.h>
 #include<Servo.h>
+Servo x;
+int bttx=2;    //tx of bluetooth module is connected to pin 9 of arduino
+int btrx=3;    //rx of bluetooth module is connected to pin 10 of arduino
+SoftwareSerial bluetooth(bttx,btrx);
